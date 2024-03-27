62f9dd3b30b52bb96133f1abd30178a7b7d9ea62
Ryuhouji <bcummans@hotmail.com>
2024-03-27 01:55:07 -0400

    fix nuzlocke freezing on capture.

--- include/constants/general.h
+++ include/constants/general.h
@@ -605,8 +605,8 @@
 //uncomment to punish save states. Otherwise detections are just logged.
 //#define RYU_PUNISH_SAVE_STATE 1
 
-#define EE_GAME_VERSION 10103
+#define EE_GAME_VERSION 10104
 //uncomment to allow dev switch to be accessed.
-//#define DEV_BUILD 69
+#define DEV_BUILD 69
 
 #endif 
